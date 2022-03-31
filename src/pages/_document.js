06f7f2d44ae807beea4cf6efa6aba3b0e2ee7db5
João Gabriel Costa <joaogabrielcosta13@icloud.com>
2022-03-31 14:07:37 -0300

    latest modifications

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/images/paper-chat-icon.png"
+          />
+          <meta name="robots" content="index, follow" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
